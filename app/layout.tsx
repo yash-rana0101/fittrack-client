1b69b4975d62bf99dd0b09f49727400a34742ba2
yash-rana0101 <ranayash812@gmail.com>
2026-05-08 18:46:44 +0530

    feat: initialize root layout with Inter font and global metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.variable} h-full antialiased`}>
+    <html lang="en" className={`${inter.variable} h-full antialiased dark`}>
       <body className="min-h-full flex flex-col">
         {children}
       </body>
